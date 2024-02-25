7c6ac78c5d3b00d9b02820e1d3aab970c15b0dfb
Serhii01010101 <jobjob2109@gmail.com>
2024-02-25 17:39:44 +0200

    create hw

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,9 @@
-const sortByDescendingFriendCount = (users) => {};
+const sortByDescendingFriendCount = (users) => {
+  return users.toSorted(
+    (firstUser, secondUser) =>
+      secondUser.friends.length - firstUser.friends.length
+  );
+};
 
 console.log(
   sortByDescendingFriendCount([
